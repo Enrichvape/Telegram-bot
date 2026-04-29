550b01454929e6050754d790f3d6127136450db2
Enrichvape <nhcsj9j7tv@privaterelay.appleid.com>
2026-04-29 14:52:57 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Bot dah hidup 🔥")
+
+bot.infinity_polling()
